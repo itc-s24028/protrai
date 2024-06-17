f47f717c6c96c49526373f93566224d7f932f63c
itc-s24028 <s24028@std.it-college.ac.jp>
2024-06-17 14:41:18 +0900

    def2.py,forsx.pyを追加

---
+++ protrai/forsx.py
@@ -0,0 +1,4 @@
+#s24028 forsx.py
+
+for i in range(30):
+    print(f"s240{i+1}")
---
+++ protrai/syouhizei.py
@@ -0,0 +1,9 @@
+# s24028 def2.py
+# 関数で消費税を計算するコード
+
+def tax(yen):
+    return yen * 1.1
+
+print(tax(120),"円")
+print(tax(128),"円")
+print(tax(980),"円")
